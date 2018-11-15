610d27fcec7ab0ee5af1a45dbb9f8ba062cbf48e
Matthew Smith <hello@mattymatt.co>
2018-11-14 20:59:24 -0500

    Updated Circles Pattern

--- Projects/HighSide/CirclesPattern/CirclesPattern.py
+++ Projects/HighSide/CirclesPattern/CirclesPattern.py
@@ -7,25 +7,39 @@ def drawEllipses(cx, cy, rx, ry, dx, dy, n):
         rx -= dx
         ry -= dy
 
-w = 800
+w = 1200
 h = 600
 canvasSize = w,h
-xPosition = randint(0, h)
+
+yPositionTop = randint(h-(h // 4), h+100)
+yPositionBottom = randint(-100, (h // 4))
+
+if yPositionTop < h+20:
+    xPositionTop = w+100
+else:
+    xPositionTop = randint((w-(w // 3)), (w+100))
+
+if yPositionBottom > -20:
+    xPositionBottom = -100
+else:
+    xPositionBottom = randint(0, w // 3)
 
 if w < h:
     radius = h
-    numEllipses = w
+    numEllipses = w // 2
 else:
     radius = w
-    numEllipses = h
+    numEllipses = ((w + 100) // 10)
 maxStep = 20
 newPage(w, h)
 frameDuration(1/20)
 fill(1,1,1)
 rect(0, 0, w, h)
 
-drawEllipses((w - xPosition), h+100, radius, radius,
+drawEllipses(xPositionTop, yPositionTop, radius, radius,
 maxStep, maxStep, numEllipses)
 
-drawEllipses(xPosition, -100, radius, radius,
+drawEllipses(xPositionBottom, yPositionBottom, radius, radius,
 maxStep, maxStep, numEllipses)
+
+saveImage("~/Desktop/HighSide_Test.pdf")
