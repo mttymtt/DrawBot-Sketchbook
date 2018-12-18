1cc42bf280fe2ec00b73e3ce12af7ee96aa787c6
Mttymtt <hello@mattymatt.co>
2018-12-18 08:04:32 -0500

    Got the calendar starting to work

--- Data/Calendar/calendar.py
+++ Data/Calendar/calendar.py
@@ -1,9 +1,20 @@
 import csv
 
-size(300,300)
+pt = 72
+
+h = 3 * pt
+w = 3 * pt
 
 with open('data.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['Year'], row['Month'], row['Day'])
-        textbox('Year', 10, 10, 280, 280)
+        # print(row['Year'], row['Month'], row['Day'])
+        year = row['Year']
+        date = row['Day']
+        newPage(h, w)
+        text(year, (10, 10))
+        fontSize(72)
+        textWidth, textHeight = textSize(date)
+        textBox(date, (0, ((w+(textHeight/-2))/-2), w, w), align="center")
+        
+        print(textHeight)
