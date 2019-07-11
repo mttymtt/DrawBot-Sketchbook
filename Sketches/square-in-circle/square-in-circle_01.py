299109f016d439a897b15bdde73575444b6ee3ca
Matthew Smith <hello@mattymatt.co>
2019-07-11 17:56:11 -0400

    Rename & update square-in-circle.py

--- Sketches/square-in-circle/square-in-circle_01.py
+++ Sketches/square-in-circle/square-in-circle_01.py
@@ -1,11 +1,14 @@
+margin = 100
+
 newPage(1000, 1000)
 fill(None)
 stroke(1,0,0)
 strokeWidth(1)
-diameter = width()
+diameter = width() - margin
 for i in range(10):    
     if i > 0:
         diameter = (diameter * sqrt(2)) - ( diameter / sqrt(2) )
     rect( ((width() - diameter) / 2), ((height() - diameter) / 2), diameter, diameter)
     oval( ((width() - diameter) / 2), ((height() - diameter) / 2), diameter, diameter)
+    
 saveImage("square-in-circle_01.png")
