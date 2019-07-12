4fd26f40d507d453ca47b563239c8afdc96aa458
Matthew Smith <hello@mattymatt.co>
2019-07-12 16:34:03 -0400

    Cosmetic update to square-in-circle.py

--- Sketches/square-in-circle/square-in-circle_01.py
+++ Sketches/square-in-circle/square-in-circle_01.py
@@ -7,7 +7,7 @@ strokeWidth(1)
 diameter = width() - margin
 for i in range(10):    
     if i > 0:
-        diameter = (diameter * sqrt(2)) - ( diameter / sqrt(2) )
+        diameter = (diameter * sqrt(2)) - (diameter / sqrt(2))
     rect( ((width() - diameter) / 2), ((height() - diameter) / 2), diameter, diameter)
     oval( ((width() - diameter) / 2), ((height() - diameter) / 2), diameter, diameter)
     
